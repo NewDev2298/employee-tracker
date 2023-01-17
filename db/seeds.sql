6df0e9d5d068d19ed57fbe0d7945e891e4e6ff14
Terrance Smith <terrancesmith@laquishas-air.mynetworksettings.com>
2023-01-16 22:48:04 -0500

    moved salaries to roles table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,45 +1,45 @@
 INSERT INTO department (name)
     VALUES
-    ("HR"),
-    ("Accounting"),
-    ("I.T."),
-    ("Sales"),
-    ("Customer Relations");
+    ("HR"), -- 1
+    ("Accounting"), --2
+    ("I.T."), --3
+    ("Sales"), --4
+    ("Customer Relations"); --5
 
 INSERT INTO role (title, salary, department_id)
     VALUES
-    ("Office Clerk"),
-    ("Administrator"),
-    ("Compliance Analyst"),
-    ("Accountant"),
-    ("Economic Analyst"),
-    ("Acct. Supervisor"),
-    ("IT Manager"),
-    ("Network Engineer"),
-    ("Security"),
-    ("Help Desk"),
-    ("Sales Associate"),
-    ("Sales Lead"),
-    ("Accounts Director"),
-    ("Support Specialist"),
-    ("Phone Operator"),
-    ("CRM")
+    ("Office Clerk", 75000, 1),
+    ("Administrator", 100000, 1),
+    ("Compliance Analyst", 75000, 1),
+    ("Accountant", 75000, 2),
+    ("Economic Analyst", 75000, 2),
+    ("Acct. Supervisor", 100000, 2),
+    ("IT Manager", 100000, 3),
+    ("Network Engineer", 75000, 3),
+    ("Security", 75000, 3),
+    ("Help Desk", 75000, 3),
+    ("Sales Associate", 75000, 4),
+    ("Sales Lead", 75000, 4),
+    ("Accounts Director", 100000, 4),
+    ("Support Specialist", 75000, 5),
+    ("Phone Operator", 75000, 5),
+    ("CRM", 100000, 5)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
     VALUES
-    ('Julius', 'Erving',75000, ),
-    ('Tim', 'Duncan', 100000, ),
-    ('Dwyane', 'Wade', 75000, ),
-    ('Candace', 'Parker', 75000, ),
-    ('Earvin', 'Johnson', 75000, ),
-    ('Chris', 'Paul', 100000, ),
-    ('Chris', 'Bosh', 100000, ),
-    ('Maya', 'Moore', 75000, ),
-    ('Hakeem', 'Olajuwon', 75000, ),
-    ('Kelsey', 'Plum', 75000, ),
-    ('Stephen', 'Curry', 75000, ),
-    ('Chris', 'Mullin', 75000, ),
-    ('Breanna', 'Stewart', 100000, ),
-    ('Lauren', 'Jackson', 75000, ),
-    ('Dirk', 'Nowitzki', 75000, )
-    ('Cheryl', 'Miller', 100000, )
+    ('Julius', 'Erving', , ),
+    ('Tim', 'Duncan', , ),
+    ('Dwyane', 'Wade', , ),
+    ('Candace', 'Parker', , ),
+    ('Earvin', 'Johnson', , ),
+    ('Chris', 'Paul', , ),
+    ('Chris', 'Bosh', , ),
+    ('Maya', 'Moore', , ),
+    ('Hakeem', 'Olajuwon', , ),
+    ('Kelsey', 'Plum', , ),
+    ('Stephen', 'Curry', , ),
+    ('Chris', 'Mullin', , ),
+    ('Breanna', 'Stewart', , ),
+    ('Lauren', 'Jackson', , ),
+    ('Dirk', 'Nowitzki', , )
+    ('Cheryl', 'Miller', , )
