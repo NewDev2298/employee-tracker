6008aab94bc1ac19dbfac292a181a5846db9ea1b
Terrance Smith <terrancesmith@laquishas-air.mynetworksettings.com>
2023-01-16 22:11:19 -0500

    started adding values into seeds file

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+    VALUES
+    ("HR"),
+    ("Accounting"),
+    ("I.T."),
+    ("Sales"),
+    ("Customer Relations");
+
+INSERT INTO role (title, salary, department_id)
+    VALUES
+    ("Office Clerk"),
+    ("Administrator"),
+    ("Compliance Analyst"),
+    ("Accountant"),
+    ("Economic Analyst"),
+    (""),
+    ("IT Specialist"),
+    ("Network Engineer"),
+    ("Security"),
+    ("Help Desk"),
+    ("Sales Associate"),
+    ("Sales Lead"),
+    ("Accounts Director"),
+    ("Support Specialist"),
+    ("Phone Opperator"),
