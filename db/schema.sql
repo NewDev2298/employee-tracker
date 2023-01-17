f3bfe63b7974ce3b74a75feb03b22de9aa3bfab0
Terrance Smith <terrancesmith@laquishas-air.mynetworksettings.com>
2023-01-16 21:12:14 -0500

    added three tables and created employees_db database

---
+++ db/schema.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS employees_db;
+
+CREATE DATABASE IF NOT EXISTS employees_db;
+
+USE employees_db;
+
+CREATE TABLE department(
+    id INT NOT NULL PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role(
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id INT 
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT
+);
