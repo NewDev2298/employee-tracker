60c7090404105927ab419fc96ad1c269f4f67dff
Terrance Smith <terrancesmith@laquishas-air.mynetworksettings.com>
2023-01-16 22:38:00 -0500

    added names and salaries for employees

--- db/seeds.sql
+++ db/seeds.sql
@@ -13,8 +13,8 @@ INSERT INTO role (title, salary, department_id)
     ("Compliance Analyst"),
     ("Accountant"),
     ("Economic Analyst"),
-    (""),
-    ("IT Specialist"),
+    ("Acct. Supervisor"),
+    ("IT Manager"),
     ("Network Engineer"),
     ("Security"),
     ("Help Desk"),
@@ -22,4 +22,24 @@ INSERT INTO role (title, salary, department_id)
     ("Sales Lead"),
     ("Accounts Director"),
     ("Support Specialist"),
-    ("Phone Opperator"),
+    ("Phone Operator"),
+    ("CRM")
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES
+    ('Julius', 'Erving',75000, ),
+    ('Tim', 'Duncan', 100000, ),
+    ('Dwyane', 'Wade', 75000, ),
+    ('Candace', 'Parker', 75000, ),
+    ('Earvin', 'Johnson', 75000, ),
+    ('Chris', 'Paul', 100000, ),
+    ('Chris', 'Bosh', 100000, ),
+    ('Maya', 'Moore', 75000, ),
+    ('Hakeem', 'Olajuwon', 75000, ),
+    ('Kelsey', 'Plum', 75000, ),
+    ('Stephen', 'Curry', 75000, ),
+    ('Chris', 'Mullin', 75000, ),
+    ('Breanna', 'Stewart', 100000, ),
+    ('Lauren', 'Jackson', 75000, ),
+    ('Dirk', 'Nowitzki', 75000, )
+    ('Cheryl', 'Miller', 100000, )
