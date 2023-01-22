78f4e0edbf8a33154ebcf341f812544e6a722d95
Terrance Smith <terrancesmith@laquishas-air.mynetworksettings.com>
2023-01-21 22:12:47 -0500

    added switch case function for view all feature

--- server.js
+++ server.js
@@ -2,17 +2,32 @@ const inquirer = require('inquirer');
 const prompt = inquirer.createPromptModule();
 const mysql = require('mysql2');
 
+require('console.table');
+
 const db = mysql.createConnection({
     user: 'root',
     database: 'employees_db'
 });
 
-db.query('SELECT * FROM department', (err, department) => {
-    console.table(department);
-});
-db.query('SELECT * FROM role', (err, role) => {
-    console.table(role);
-});
-db.query('SELECT * FROM employee', (err, employee) => {
-    console.table(employee);  
-});
+
+// INSTRUCTOR HELPED GUIDE SWITCH CASE FUNCTION
+
+const options =  (type) => {
+    switch (type) {
+        case 'VIEW ALL DEPARTMENTS':{
+            db.query('SELECT * FROM department', (err, department) => {
+                console.table(department);
+            });
+        }
+        case 'VIEW ALL ROLES':{
+            db.query('SELECT * FROM role', (err, role) => {
+                console.table(role);
+            });
+        }
+        case 'VIEW ALL EMPLOYEES':{
+            db.query('SELECT * FROM employee', (err, employee) => {
+                console.table(employee);  
+            });
+        }
+    };
+};
